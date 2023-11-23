98c9ebe645b3c57242df854849c2d19a3f54c9a6
PAVANSAI2468 <pavansarapu85@gmail.com>
2023-11-23 17:24:42 +0000

    adding app.js file

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const dbpath = path.join(__dirname, "userData.db");
+const app = express();
+app.use(express.json());
+let db = null;
+const bcrypt = require("bcrypt");
+const intailiseServer = async () => {
+  try {
+    db = await open({
+      filename: dbpath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("server is running");
+    });
+  } catch (e) {
+    console.log(`db error:${e.message}`);
+    process.exit(1);
+  }
+};
+intailiseServer();
+app.post("/register", async (req, res) => {
+  const { username, name, password, gender, location } = req.body;
+  const hashPassword = await bcrypt.hash(password, 10);
+  const getUsername = `select username from user where username='${username}';`;
+  const fromDB = await db.get(getUsername);
+  if (fromDB !== undefined) {
+    res.status(400);
+    res.send("User already exists");
+  } else {
+    if (password.length < 5) {
+      res.status(400);
+      res.send("Password is too short");
+    } else {
+      res.status(200);
+      res.send("User created successfully");
+    }
+  }
+});
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const getUsername = `select username from user where username='${username}';`;
+  const fromDB = await db.get(getUsername);
+  if (fromDB === undefined) {
+    res.status(400);
+    res.send("Invalid user");
+  } else {
+    const isCorrectPassword = await bcrypt.compare(password, fromDB.password);
+    if (isCorrectPassword) {
+      res.status(200);
+      res.send("Login success!");
+    } else {
+      res.status(400);
+      res.send("Invalid password");
+    }
+  }
+});
+app.put("/change-password", async (req, res) => {
+  const { username, oldPassword, newPassword } = req.body;
+  const getPassword = `select password from user where username='${username}';`;
+  const fromdb = await db.get(getPassword);
+  if (fromdb !== oldPassword) {
+    res.status(400);
+    res.send("Invalid current password");
+  } else {
+    if (newPassword.length < 5) {
+      req.status(400);
+      req.send(" Password is too short");
+    } else {
+      const hashNewPassword = await bcrypt.hash(newPassword, 10);
+      const updatePassword = `UPDATE user SET password='${newPassword}'`;
+      res.status(200);
+      res.send("Password updated");
+    }
+  }
+});
+module.exports = app;
